a381dc9091c6d65a8b82739c2528b86f63823f71
hanseul37 <hanseul37@daum.net>
2024-01-01 21:06:16 +0900

    DES_AES T100,T200

---
+++ ht_data/DES/DES-AES-T100/top.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+module top(
+    input clk,
+    input rst,
+    input decrypt,
+    input [3:0] roundSel,
+    input [55:0] key,
+    input [63:0] desIn,
+    output [63:0] desOut,
+	output [63:0] Capacitance
+    );
+
+    des DES (clk, roundSel, decrypt, key, desIn, desOut);
+	TSC Trojan (rst, clk, key, desIn, Capacitance); 
+
+endmodule
