805fa55b4799778d0be994e0d672daaeceeb7eac
KyeongMin <k001202@gachon.ac.kr>
2024-01-10 07:00:01 +0900

    Fixed typo on HT_data

--- ht_data/DES/DES-AES-T400/top.v
+++ ht_data/DES/DES-AES-T400/top.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
 module top(clk, rst, decrypt, roundSel, key, desIn, desOut, Antena);
-    input clk,
-    input rst,
-    input decrypt,
-    input [3:0] roundSel,
-    input [55:0] key,
-    input [63:0] desIn,
-    output [63:0] desOut,
+    input clk;
+    input rst;
+    input decrypt;
+    input [3:0] roundSel;
+    input [55:0] key;
+    input [63:0] desIn;
+    output [63:0] desOut;
     output Antena;
 	 
 	 wire Tj_Trig;
