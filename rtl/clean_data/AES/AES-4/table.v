384023cf7213368558d55c6e7a1b55916ce8d12a
KyeongMin <k001202@gachon.ac.kr>
2024-02-06 01:21:48 +0900

    Fixed AES-1 and AES-4

--- rtl/clean_data/AES/AES-4/table.v
+++ rtl/clean_data/AES/AES-4/table.v
@@ -19,12 +19,17 @@ module table_lookup (clk, state, p0, p1, p2, p3);
     input [31:0] state;
     output [31:0] p0, p1, p2, p3;
     wire [7:0] b0, b1, b2, b3;
+	
+	wire [31:0] new_p0, new_p1, new_p2;
+	assign p0 = {new_p0[7:0], new_p0[31:8]};
+	assign p1 = {new_p1[15:0], new_p1[31:16]};
+	assign p2 = {new_p2[23:0],  new_p2[31:24]};
     
     assign {b0, b1, b2, b3} = state;
     T
-        t0 (clk, b0, {p0[23:0], p0[31:24]}),
-        t1 (clk, b1, {p1[15:0], p1[31:16]}),
-        t2 (clk, b2, {p2[7:0],  p2[31:8]} ),
+        t0 (clk, b0, new_p0),
+        t1 (clk, b1, new_p1),
+        t2 (clk, b2, new_p2),
         t3 (clk, b3, p3);
 endmodule
 
