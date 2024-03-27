e47f2fdbbdd3ef5d618829954143022a4e625c74
hanseul37 <hanseul37@daum.net>
2024-03-27 13:32:39 +0900

    [FIX] C htdata XTEA-1 strlen

--- C/ht_data/XTEA/XTEA-1-AES-T100/top.c
+++ C/ht_data/XTEA/XTEA-1-AES-T100/top.c
@@ -3,9 +3,17 @@
 
 #include "xtea.h"
 
+size_t length(const char *str) {
+    size_t len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
 void top(unsigned char key[16], unsigned char text[16], unsigned char load[32], xtea_t xtea, char encode[64], unsigned char iv[8], int enclen){
     xtea_setkey(&xtea, key);
-    enclen = xtea_enclen((int)strlen(text));
+    enclen = xtea_enclen((int)length(text));
 
     set(iv, 0, sizeof(iv));
     enclen = xtea_encode(&xtea, encode, text, (int)strlen(text), iv);
