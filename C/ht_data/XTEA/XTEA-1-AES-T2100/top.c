82c3f8011123fb59ce845505a0817890c34d01f6
hanseul37 <hanseul37@daum.net>
2024-04-02 13:03:06 +0900

    C htdata RC5, XTEA 600, 2000, 2100

---
+++ C/ht_data/XTEA/XTEA-1-AES-T2100/top.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "xtea.h"
+
+size_t length(const char *str) {
+    size_t len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+void top(unsigned char key[16], unsigned char text[16], xtea_t xtea, char encode[64], unsigned char iv[8], int enclen, unsigned char INV_out[11], unsigned char counter){
+    xtea_setkey(&xtea, key);
+    enclen = xtea_enclen((int)length(text));
+
+    set(iv, 0, sizeof(iv));
+    enclen = xtea_encode(&xtea, encode, text, (int)length(text), iv);
+
+    tsc(text, INV_out, counter);
+}
